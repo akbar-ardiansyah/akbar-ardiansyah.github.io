382ba4c3898074702857e8ac635dc1e8f5a940f7
akbar-ardianyah <akbarardiansyah00@gmail.com>
2024-05-01 21:03:31 +0700

    restilyng

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,10 @@ document.querySelector('.bullet-red').addEventListener('click', function () {
         // container.classList.remove('flip');
     }, 1000); // Adjust the delay according to your preference
 });
+
+document.getElementById('twitter-card').addEventListener('mouseover', function () {
+    $('#twitter-modal').modal('show');
+    $('#twitter-modalLabel').text(' ');
+    $('.modal-body').html('<iframe src="https://twitter.com/kbarvds" width="100%" height="500" title="Twitter Feed" frameborder="0" scrolling="no"></iframe>');
+
+});
